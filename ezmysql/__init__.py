c86698669381e7c6bd70ea7a4a791061675e2b01
veelion <veelion@gmail.com>
2023-02-25 18:04:07 +0800

    upgrade to 0.10.1

--- ezmysql/__init__.py
+++ ezmysql/__init__.py
@@ -1,6 +1,7 @@
 # encoding: utf8
 
-__version__ = '0.9.0'
+__version__ = '0.10.1'
 
 from .connection_sync import ConnectionSync
 from .connection_async import ConnectionAsync
+from .connection_oracle import ConnectionOracle
